a859bff52cdd0098029dcea63ed60889b70ea43e
MateoRH <marami26@eafit.edu.co>
2019-11-19 09:15:39 -0500

    Agregando estructura front

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,27 @@
-$('.message a').click(function() {
-  $('form').animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+const form = document.querySelector('.login-form');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
 });
+
+function login() {
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: 'http://localhost:3000/login',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: {
+      email: email.value,
+      password: password.value
+    }
+  };
+
+  $.ajax(settings).done(function(response) {
+    console.log(response);
+  });
+}
---
+++ public/js/routes.js
@@ -0,0 +1,19 @@
+function routeRegister() {
+  window.location.href = 'register.html';
+}
+
+function routeLogin() {
+  window.location.href = 'index.html';
+}
+
+function routeOpinion() {
+  window.location.href = 'opinion.html';
+}
+
+function routeProduct() {
+  window.location.href = 'product.html';
+}
+
+function routeError() {
+  window.location.href = 'error.html';
+}
